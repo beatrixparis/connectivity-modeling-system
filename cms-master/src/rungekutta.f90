c37f13f39a9459faccb9d7eed4d4e2702a52bcf0
Eric Firing <efiring@hawaii.edu>
2019-04-06 16:45:20 -1000

    Add config for profiling; don't calculate density if not needed

--- cms-master/src/rungekutta.f90
+++ cms-master/src/rungekutta.f90
@@ -2,8 +2,8 @@
 !* System: Connectivity Modeling System (CMS)                               *
 !* File : rungekutta.f90                                                    *
 !* Last Modified: 2016-07-22                                                *
-!* Code contributors: Claire B Paris, Ana Carolina Vaz, Judith Helgers,     * 
-!*                    Ashwanth Srinivasan, Erik van Sebille                 * 
+!* Code contributors: Claire B Paris, Ana Carolina Vaz, Judith Helgers,     *
+!*                    Ashwanth Srinivasan, Erik van Sebille                 *
 !*                                                                          *
 !* Copyright (C) 2011, University of Miami                                  *
 !*                                                                          *
@@ -43,7 +43,7 @@
  logical (kind=log_kind), intent(out) :: flag(6)
 
  real (kind = real_kind) :: grid_i(QAx),grid_j(QAx),grid_k(QAx), & !position of particle in i,j,k
-     w2, & !for buoyancy 
+     w2, & !for buoyancy
      ssh1,ssh2, & !for tidalMovement
      value
  logical (kind=log_kind) :: fail, landFlag2, landFlag1
@@ -65,22 +65,22 @@
  IF (nextFile(rk_step) .eqv. .true.) THEN
 ! start at fname(2) for this runga kutta step
   firstFile = 2
- ELSE 
+ ELSE
 ! standard is to start at fname(1)
   firstFile = 1
  ENDIF
-       
+
 
 !calculate U-velocity
  CALL fld3_interp(grid_i(UAx), grid_j(UAx), grid_k(UAx), nests(ngrid)%idm(UAx), &
    nests(ngrid)%jdm(UAx), nests(ngrid)%kdm(UAx), &
    nests(ngrid)%uvel(:,:,:,firstFile:firstFile+n_weight-1), &
    nests(ngrid)%w(:,rk_step), n_weight, 2.0**19, &
-   ngrid, value, fail, landFlag1)  
+   ngrid, value, fail, landFlag1)
  IF (fail .or. abs(value)>100.0) THEN
   u=0.0
  ELSE
-  u=value 
+  u=value
  ENDIF
  flag(1)=fail
 
@@ -99,28 +99,28 @@
 
 !check if particle is too close to or on land
   IF (landFlag) THEN
-! if subroutine is only called to check if particle is on land then 
+! if subroutine is only called to check if particle is on land then
 ! check if particle is on land and then return to subroutine move
    IF ((landFlag2) .or. (landFlag1)) THEN
     landFlag = .true.
-   ELSE 
+   ELSE
     landFlag = .false.
    ENDIF
-   RETURN 
-  ELSE 
+   RETURN
+  ELSE
    IF ((landFlag2) .or. (landFlag1)) THEN
     landFlag = .true.
-   ELSE 
+   ELSE
     landFlag = .false.
    ENDIF
   ENDIF
 
-!calculate W-velocity     
+!calculate W-velocity
  IF (ibio) THEN
 ! if ibio = true then the W-velocity is not used
   flag(3) = .false.
   w = 0.0
- ELSE  
+ ELSE
   CALL fld3_interp(grid_i(WAx), grid_j(WAx), grid_k(WAx), &
     nests(ngrid)%idm(WAx), nests(ngrid)%jdm(WAx), nests(ngrid)%kdm(WAx), &
     nests(ngrid)%wvel(:,:,:,firstFile:firstFile+n_weight-1), &
@@ -135,7 +135,7 @@
  ENDIF
 
  IF (withibm) THEN
-!calculate Temperature, only necessary if buoyancy or mortality is used 
+!calculate Temperature, only necessary if buoyancy or mortality is used
  IF ((buoyancy) .or. (mort) .or. (outputtemp)) THEN
   CALL fld3_interp(grid_i(QAx), grid_j(QAx), grid_k(QAx), &
     nests(ngrid)%idm(QAx), nests(ngrid)%jdm(QAx), nests(ngrid)%kdm(QAx), &
@@ -171,8 +171,8 @@
   s = 0.0
  ENDIF
 
-!calculate Density, only necessary if using buoyancy      
- IF (buoyancy) THEN
+!calculate Density, only necessary if using buoyancy
+ IF (buoyancy .or. massSpawning) THEN
   CALL fld3_interp(grid_i(QAx), grid_j(QAx), grid_k(QAx), &
     nests(ngrid)%idm(QAx), nests(ngrid)%jdm(QAx), nests(ngrid)%kdm(QAx), &
     nests(ngrid)%dens(:,:,:,firstFile:firstFile+n_weight-1), &
@@ -181,7 +181,7 @@
   IF (fail)  THEN
    rho=0.0
   ELSE
-   rho=value     
+   rho=value
   ENDIF
   flag(6)=fail
  ELSE
@@ -189,14 +189,14 @@
   rho = 0.0
  ENDIF
 
-!Adding buyancy component only if there is a t and rho    
+!Adding buyancy component only if there is a t and rho
  IF (buoyancy) THEN
   IF (rho .gt. 0. .and. t .gt. 0.) THEN
    IF (rho .lt. 1000.) THEN
       rho = rho + 1000.
    ENDIF
    CALL get_buoyancy(rho, t,diam_part, dens_part, w2)
-   w=w + w2      
+   w=w + w2
   ENDIF
  ENDIF
 
@@ -204,7 +204,7 @@
 !if the sea surface height rises then the shrimp can move
 !if the sea surface height drops then the shrimp cannot move
  IF (tidalMovement) THEN
-   notmove = .false. 
+   notmove = .false.
    IF (run_time .gt. (tstStart*secs_in_day)) THEN
      CALL fld2d_interp(grid_i(QAx), grid_j(QAx), nests(ngrid)%idm(QAx), &
        nests(ngrid)%jdm(QAx), nests(ngrid)%ssh(:,:,1), 2.0**19, ngrid, ssh1, fail)
@@ -213,7 +213,7 @@
      IF (ssh1 .ge. ssh2) THEN
        u = 0.0
        v = 0.0
-       w = 0.0 
+       w = 0.0
        notmove = .true.
      ENDIF
    ENDIF
