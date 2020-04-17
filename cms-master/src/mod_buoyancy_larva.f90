9f7f65359fc9a846643228467ee4dc76abf9defe
anacarolvaz <anacarolvaz@gmail.com>
2020-04-17 18:24:31 -0400

    Update mod_buoyancy_larva.f90
    Simplify Stokes equation calculation

--- cms-master/src/mod_buoyancy_larva.f90
+++ cms-master/src/mod_buoyancy_larva.f90
@@ -39,43 +39,21 @@ CONTAINS
 !**************************************************************
 !Calculate buoyancy component for larva
 !Equation (2), (3) and (4) from Zheng and Yapa(2000)
-subroutine get_buoyancy(dens_water, temp_water,diam_part, dens_part, w2)
+subroutine get_buoyancy(dens_water, temp_water, diam_part, dens_part, w2)
 
 real(kind=real_kind), intent(in)  :: &
      dens_water, temp_water, diam_part, dens_part
 real(kind=real_kind), intent(out) :: w2
 
-real(kind=real_kind) :: &
-     viscosity,dc1, dc2, dc, power, wpart1, wpart2, wpart3
+real(kind=real_kind) :: viscosity
 
  viscosity = 1.88e-3 - (temp_water*4.e-5) 
- !print *, "dens water: ", dens_water
- !print *, "viscosity water: ", viscosity   
- !print *, "diameter particle: ", diam_part
- !print *, "density particle: ", dens_part
 
-!Equation (4) from Zheng and Yapa(2000)
- power = 2./3.
- dc1 = (9.52 * viscosity)**(power)
- power = 1./3.  
- dc2 = (9.8 * dens_water * abs(dens_part-dens_water))**(power)
- dc = dc1/dc2
- !print *, dc
-
- IF (diam_part .lt. dc) THEN
-!Equation (2) from Zheng and Yapa(2000). Formula of stokes  
-  w2=1. * (9.8 * (diam_part*diam_part) * (dens_part-dens_water)) /(18. * viscosity)
-  !print *, "stokes: ",w2
- ELSE
-!Equation (3) from Zheng and Yapa(2000).
-  wpart1 = -8. * 9.8 * diam_part * (dens_part-dens_water)
-  wpart2 = 3. * dens_water
-  wpart3 = wpart1/wpart2 
-  w2 = -1. * sqrt(wpart3)
-  !print *, "other formula: ",diam_part, w2   
- ENDIF
-
+ !Formula of Stokes  
+  w2 = (9.8*(diam_part*diam_part)*(dens_part-dens_water))/(18.*viscosity)
+  
 END SUBROUTINE get_buoyancy
+
 !**************************************************************
  subroutine load_buoyancy(buoyancyFilename)
 
